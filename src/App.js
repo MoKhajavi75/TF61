4d22ab2cd7a180060cde36c50a36e49f81eada01
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-12-06 11:41:33 +0330

    Add react-native-linear-gradient

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 46, fontFamily: "Vazir Black FD" }}>تاپ فن</Text>
+      <TouchableOpacity
+        style={{
+          width: 300,
+          height: 95,
+          borderRadius: 10
+        }}
+      >
+        <LinearGradient
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "stretch",
+            borderRadius: 10
+          }}
+          colors={["#159", "#789"]}
+        >
+          <Text
+            style={{
+              fontSize: 46,
+              fontFamily: "Vazir Black FD",
+              color: "white"
+            }}
+          >
+            تاپ فن
+          </Text>
+        </LinearGradient>
+      </TouchableOpacity>
     </View>
   );
 };
