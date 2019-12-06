5ac9caeabf5d8095ddcb763233be66d40d3f7db8
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-12-06 11:20:22 +0330

    Add font

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const App = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Top Fan</Text>
+      <Text style={{ fontSize: 46, fontFamily: "Vazir Black FD" }}>تاپ فن</Text>
     </View>
   );
 };
