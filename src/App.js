76bdbabff99c6f779375c8227fcc37b25e688866
MohamadKh75 <khajavi_mohamad@yahoo.com>
2019-12-06 11:14:52 +0330

    Move App to src folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Top Fan</Text>
+    </View>
+  );
+};
+
+export default App;
